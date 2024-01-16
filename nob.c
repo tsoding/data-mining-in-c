31703b2dd9e72d6c95353d71329aedff41d15b58
rexim <reximkut@gmail.com>
2024-01-16 22:44:02 +0700

    Parallelize the build

--- nob.c
+++ nob.c
@@ -1,7 +1,7 @@
 #define NOB_IMPLEMENTATION
 #include "./src/nob.h"
 
-bool build_program(const char *source_path, const char *output_path)
+Nob_Proc build_program(const char *source_path, const char *output_path)
 {
     Nob_Cmd cmd = {0};
     nob_cmd_append(&cmd, "cc");
@@ -12,15 +12,17 @@ bool build_program(const char *source_path, const char *output_path)
     nob_cmd_append(&cmd, source_path);
     nob_cmd_append(&cmd, "-L./raylib/", "-L./zlib/");
     nob_cmd_append(&cmd, "-lraylib", "-lm", "-lz", "-ldl", "-lpthread");
-    return nob_cmd_run_sync(cmd);
+    return nob_cmd_run_async(cmd);
 }
 
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
     if (!nob_mkdir_if_not_exists("./build/")) return 1;
-    // if (!build_program("./src/2d.c", "./build/2d")) return 1;
-    // if (!build_program("./src/3d.c", "./build/3d")) return 1;
-    if (!build_program("./src/knn.c", "./build/knn")) return 1;
+    Nob_Procs procs = {0};
+    nob_da_append(&procs, build_program("./src/2d.c", "./build/2d"));
+    nob_da_append(&procs, build_program("./src/3d.c", "./build/3d"));
+    nob_da_append(&procs, build_program("./src/knn.c", "./build/knn"));
+    if (!nob_procs_wait(procs)) return 1;
     return 0;
 }
